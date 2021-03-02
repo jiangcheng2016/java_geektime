001a44e4f28d3713ac2732c65ae9565e3ead2ec7
jiangcheng2016 <jiangchengc2016@163.com>
2021-03-02 18:37:16 +0800

    class_ChapterTwo

---
+++ src/MerchandiseUsingVariableEample.java
@@ -0,0 +1,8 @@
+public class MerchandiseUsingVariableEample {
+    public static void main(String[] args) {
+        String m1Name = "茉莉花茶叶20包";
+        int m1Count = 20;
+        System.out.println(1);
+
+    }
+}
---
+++ src/towPasseager/Merchandise.java
@@ -0,0 +1,14 @@
+package towPasseager;
+
+// >> TODO 一个类以public class 开头， public class 代表这个类是公共类，类名必须和文件名相同。
+// >> TODO public class 后面紧跟类名，然后是一对大括号的类体
+public class Merchandise {
+    // >> TODO 类体中可以定义描述这个类的属性的变量。我们称之为成员变量(member variable)
+    // >> TODO 每个成员变量的定义以 ; 结束
+    String name;
+    String id;
+    int count;
+    double price;
+}
+
+// >> TODO 上面这整个类，其实就是创建了一个模板。描述了一种我们需要的数据类型。
