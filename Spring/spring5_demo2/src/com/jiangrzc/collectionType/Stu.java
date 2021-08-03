d8e3dd19d19a0c8cbe9c82ecda3392dbc1e98693
jiangcheng2016 <jiangchengc2016@163.com>
2021-08-03 10:51:25 +0800

    spring_demo2

--- Spring/spring5_demo2/src/com/jiangrzc/collectionType/Stu.java
+++ Spring/spring5_demo2/src/com/jiangrzc/collectionType/Stu.java
@@ -1,4 +1,44 @@
 package com.jiangrzc.collectionType;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 创建 Stu 类，定义数组、List、Map、Set类型属性，生成set方法
+ */
 public class Stu {
+
+    //1.数组类型
+    private String[] courses;
+    //2.List集合
+    private List<String> list;
+    //3.Map集合
+    private Map<String,String> maps;
+    //4.set集合
+    private Set<String> sets;
+
+    public void setCourses(String[] courses) {
+        this.courses = courses;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public void setMaps(Map<String, String> maps) {
+        this.maps = maps;
+    }
+
+    public void setSets(Set<String> sets) {
+        this.sets = sets;
+    }
+
+    public void show(){
+        System.out.println("courses:" + Arrays.toString(courses));
+        System.out.println("list:" + list);
+        System.out.println("maps:" + maps);
+        System.out.println("sets:" + sets);
+    }
 }
--- Spring/spring5_demo2/src/com/jiangrzc/test/TestBean.java
+++ Spring/spring5_demo2/src/com/jiangrzc/test/TestBean.java
@@ -1,4 +1,24 @@
 package com.jiangrzc.test;
 
+import com.jiangrzc.collectionType.Stu;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestBean {
+
+    @Test
+    public void testBean(){
+
+        //1.获取spring配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+
+        //2.获取配置创建的对象
+        Stu stu = context.getBean("stu", Stu.class);
+
+        //操作
+        System.out.println(stu);
+
+        stu.show();
+    }
 }
