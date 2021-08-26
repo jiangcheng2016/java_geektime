dde5ba5b3563a0a802f4baca95ead88aa38d1d68
jiangcheng2016 <jiangchengc2016@163.com>
2021-08-26 22:14:11 +0800

    teststring

---
+++ recruitment/src/com/jiangrzc/test/TestString.java
@@ -0,0 +1,17 @@
+package com.jiangrzc.test;
+
+public class TestString {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 2;
+
+//        System.out.println(a.equals(b));
+
+        String c = "2";
+        String d = new String("2");
+
+        System.out.println(c == d);
+    }
+}
